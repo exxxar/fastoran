094906f876a8141a6a6b7c4b10ad5eae4206aad0
exxxar <exxxar@gmail.com>
2020-03-23 00:49:43 +0300

    заполнение кухонь

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <link href='http://fastoran.com/css/easydropdown.css' rel='stylesheet' type='text/css'>
     <link href='http://fastoran.com/css/owl.carousel.css' rel='stylesheet' type='text/css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="{{asset('js/app.js')}}"></script>
 
 </head>
 <body>
