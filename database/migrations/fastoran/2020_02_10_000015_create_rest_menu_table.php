57965f25b131fa04b27ea3e9a793770b9e031e83
exxxar <exxxar@gmail.com>
2020-03-30 20:12:49 +0300

    добавил вояджер

--- database/migrations/fastoran/2020_02_10_000015_create_rest_menu_table.php
+++ database/migrations/fastoran/2020_02_10_000015_create_rest_menu_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMenuTable extends Migration
+class CreateRestMenuTable extends Migration
 {
     /**
      * Schema table name to migrate
