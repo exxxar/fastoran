760cfb7d7926522914b636ced374a6c5590cd57b
exxxar <exxxar@gmail.com>
2020-03-30 19:03:33 +0300

    правки по таблице пользователя

--- database/migrations/fastoran/2020_03_30_144401_create_ratings_table.php
+++ database/migrations/fastoran/2020_03_30_144401_create_ratings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRaitingsTable extends Migration
+class CreateRatingsTable extends Migration
 {
 
     public $tableName = 'ratings';
