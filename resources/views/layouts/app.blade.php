14233fd12a02ed10eda611eb37f985cde253ac6c
exxxar <exxxar@gmail.com>
2020-03-30 19:51:05 +0300

    правки по стилям

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,11 @@
     <meta name="description"
           content="Быстрый и удобный сервис доставки еды со всех ресторанов города, суши, пицца, бургеры и много другое прямо к Вам домой"/>
 
-    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
+    <link href='http://fastoran.com/css/easydropdown.css' rel='stylesheet' type='text/css'>
+    <link href='http://fastoran.com/css/owl.carousel.css' rel='stylesheet' type='text/css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
 
 
 </head>
