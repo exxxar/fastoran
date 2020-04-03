6c8fbef675e40dc88552bf11946f2aa080a092ef
exxxar <exxxar@gmail.com>
2020-04-03 11:14:14 +0300

    правки по главной странице

--- resources/views/fastoran/partials/food__special__offer.blade.php
+++ resources/views/fastoran/partials/food__special__offer.blade.php
@@ -114,8 +114,9 @@
                         </div>
                         <div class="banner__hover__action banner__left__bottom">
                             <div class="banner__hover__inner">
+                                <h4>{{$product->food_price}}₽</h4>
                                 <h2 class="pink-text">{{$product->food_name}}</h2>
-                                <p>{{$product->food_price}}₽</p>
+
                             </div>
                         </div>
                     </div>
