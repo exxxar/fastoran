b82a705d24eee32708e4bf78b1cafe5a5fccfdd0
exxxar <exxxar@gmail.com>
2020-04-07 20:40:14 +0300

    правки очередные

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Parts\Models\Fastoran\Kitchen;
+use App\Parts\Models\Fastoran\Restoran;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +20,21 @@ class AppServiceProvider extends ServiceProvider
         //
 
         Schema::defaultStringLength(191);
+
+        try {
+            $kitchens = (Kitchen::where("is_active", 1)
+                ->get())->filter(function ($kitchen) {
+                return $kitchen->rest_count > 0;
+            });
+
+            $restorans = Restoran::where("moderation", true)->get();
+
+            View::share('kitchens', $kitchens);
+            View::share('restorans', $restorans);
+        } catch (\Exception $e) {
+
+        }
+
     }
 
     /**
