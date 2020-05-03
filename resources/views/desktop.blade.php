6772b23e244a811b473e3e2f3b9c1e32da6aa112
exxxar <exxxar@gmail.com>
2020-05-04 00:12:01 +0300

    правки пва

--- resources/views/desktop.blade.php
+++ resources/views/desktop.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Fastoran - твоя доставка вкусной еды!</title>
-    <link rel="stylesheet" href="{{asset('css/desktop/desktop.css')}}">
+    <link rel="stylesheet" href="{{asset('css/desktop/desktop.css')}}?ver={{env("APP_VERSION")}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="description" content="Bitter Bootstrap 4 Based Mobile Template">
     <meta name="keywords"
