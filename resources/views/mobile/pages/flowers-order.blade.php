4ba1feb5767f96ec4a05cb64afb48b879e4f1858
exxxar <exxxar@gmail.com>
2020-07-08 18:59:03 +0300

    pwa fixes

--- resources/views/mobile/pages/flowers-order.blade.php
+++ resources/views/mobile/pages/flowers-order.blade.php
@@ -4,10 +4,10 @@
     <div class="mt-2"></div>
     @include("mobile.partials.main.section_title",[ "title"=>"Цветочная лавка", "muted"=>"Цветы на выбор"])
     <h6 class="text-center">Теперь каждый пользователь сможет посетить нашу цветочную лавку!</h6>
-    <a href="/m/restoran/ona_hochet_dn" class="mb-1" style="display: block" title="Цветочная лавка" aria-label="Цветочная лавка">
+    <a href="/m/restoran/ona_hochet_dn"  style="display: block" title="Цветочная лавка" >
         <img src="/images/fastoran/restorans/flowers.jpg" alt="Цветы Fastoran" class="img-fluid">
     </a>
-    <a href="/m/restoran/ona_hochet_dn" class="mb-2 btn btn-outline-info" style="display: block" title="Цветочная лавка" aria-label="Цветочная лавка">
+    <a href="/m/restoran/ona_hochet_dn" class="mb-2 btn btn-outline-info" title="Цветочная лавка" >
        Цветочная лавка
     </a>
     <h6 class="text-center mb-2">Либо, закажите звонок и наши флористы подберут букет под ваши пожелания!</h6>
