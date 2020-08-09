82a2067edc1418ef40c63db4f00b7761bfd1576a
exxxar <exxxar@gmail.com>
2020-08-09 12:43:21 +0300

    fixes

--- resources/views/mobile/pages/restorans.blade.php
+++ resources/views/mobile/pages/restorans.blade.php
@@ -5,6 +5,7 @@
 
     @include("mobile.partials.main.section_title",[ "title"=>"Доступные заведения", "muted"=>"Партнеры"])
     <mobile-restorans :restorans="{{$restorans}}"></mobile-restorans>
+    <div class="mt-2"></div>
     @include("mobile.partials.main.section_title",[ "title"=>"Товары", "muted"=>"Другие направления"])
     <mobile-sections></mobile-sections>
     @include("mobile.partials.bottom_menu",["active"=>3])
